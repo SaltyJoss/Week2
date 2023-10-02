b5e1e98fd9cc02f0e913594adef682594b449dc8
SaltyJoss <145990319+SaltyJoss@users.noreply.github.com>
2023-10-02 15:02:11 +0100

    Week 2 Monday Lesson work

---
+++ Week_2.py
@@ -0,0 +1,143 @@
+#TASK
+
+#total = 100
+#print(total)
+
+#total = 100
+#total = total + 99
+#print ("The total is now", total)
+
+#total = 100
+#total += 99
+#print("The total is now", total)
+
+#total = 100
+#total *= 100
+#print("The total is now", total)
+
+#value = 10
+#total = value + 90
+#print(total)
+
+#TASK
+
+#total = 756
+#total -= 1
+#print("The total is", total)
+
+#total = 666
+#total *= 4
+#print("The total is", total)
+
+#total = 5
+#total /= 2
+#print("The total is", total)
+
+#TASK
+
+#total = 98.2
+#count = 5
+#average = 98.2 / 5
+
+#print(f"The average number of the total {total}, and, {count} counts' is,", average)
+
+#TASK
+
+#type(10.5) - float
+#type(10) - integer
+#total = 10
+#type(total) - integer
+#type(total * 2) - integer
+
+#TASK
+
+#type(False) - Bool
+#type(1000) - integer
+#type(100.111) - float
+#type("Hello") - str
+#type(True) - bool
+#type(100 / 5) - float
+#type(100 // 5) - integer
+
+#DID IT ON PYTHON TERMINAL
+#10 + 10 = 20
+#"10" + "10" = '1010'
+#"ABC" * 10 = 'ABCABCABCABCABCABCABCABCABCABC'
+
+#TASK Write code that calls print function mutlitple times.
+
+#if __name__ == '__main__':
+#    name = input('Hello, what is your name? ')
+#    postcode = input(f"Hello {name}. What's your postcode?")
+#    number = int(input(f"Hello {name}. What's your phone number?"))
+
+#    Question = str(input("Would you like me to repeat your name, postcode and phone number? Answer True or False?"))
+
+#    if Question == "True" or "true" or "TRUE":
+#        print(f"Your name is {name}, your postcode is {postcode} and your phone number is {number}.")
+#    if Question == "False" or "false" or "FALSE":
+#        print("Okay goodbye", exit)
+#    if Question != "True" or "true" or "TRUE" or "False" or "FALSE" or "false":
+#        print("That's not a valid answer, goodbye")
+
+#TASK
+
+#num1 = float(input("Input your first number:"))
+#num2 = float(input("Input your second number:"))
+
+#print("Your numbers multiplied is:", num1 * num2)
+
+#TASK
+
+#comment = "I would've "thought" you knew better"
+#Synatx error
+
+#TASK
+
+#print('This text includes characters such as \'\\\' \'\"\' and \"\'\".\n\tThis is a new that starts with a tab'
+#      '\n\t\tThis is a new that starts with two tabs')
+
+#TASK
+
+#print("\ " * 54, '\nHello World!\n', "\ " * 54)
+
+#TASK
+
+#print("""This text spans three lines\nand includes both single ('),\nand double quotes (").""")
+
+#TASK
+
+#surname = "Palin"
+#intitial = surname[3]
+#print(intitial)
+
+#surname = "Palin"
+#intitial = surname[10]
+#string index out of range
+
+#surname = "Palin"
+#last = surname[-2]
+#print(last)
+
+#TASK
+
+#surname = "Palin"
+#middle = surname[1:5]
+#print(middle)
+
+#surname = "Palin"
+#last_one = surname[:-1]
+#print(last_one)
+
+#TASK
+
+#primes = [2, 3, 5, 7, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+#first_four = primes[0:4]
+#print(first_four)
+
+#names = ["Tom"]
+#names[0:0] = ["Tim", "Adam"]
+#print(names)
+
+
+
